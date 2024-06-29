4baa9ff2d51d9a8951545f0845989da3e1cd13f4
Sharifjon <sharifboymuminov@gmail.com>
2024-06-29 20:49:10 +0500

    good :)

---
+++ lib/data/models/notes/notes_model.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class NotesModel {
+  final String title;
+  final Color color;
+
+  NotesModel({required this.color,required this.title});
+
+}
---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:notesapp/data/models/notes/notes_model.dart';
+import 'package:notesapp/utils/app_size.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final List<NotesModel> items = [
+    NotesModel(color: Colors.redAccent, title: 'Savva'),
+    NotesModel(color: Colors.pinkAccent, title: 'Olluco'),
+    NotesModel(color: Colors.deepPurpleAccent, title: 'Lona'),
+    NotesModel(color: Colors.blue, title: 'Folk'),
+    NotesModel(color: Colors.lightBlueAccent, title: 'White Rabbit'),
+    NotesModel(color: Colors.green, title: 'Sage'),
+    NotesModel(color: Colors.orangeAccent, title: 'Maya'),
+    NotesModel(color: Colors.orange, title: 'Jun'),
+    NotesModel(color: Colors.deepPurple, title: 'Onset'),
+    NotesModel(color: Colors.lightBlueAccent, title: 'Probka на Цветном'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    width = MediaQuery.of(context).size.width;
+    height = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      backgroundColor: const Color(0xFF373A40),
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF373A40),
+        leading: IconButton(
+          onPressed: () {},
+          icon: SvgPicture.asset(
+            "assets/icons/setting.svg",
+            width: 24.we,
+            height: 24.we,
+            colorFilter: const ColorFilter.mode(Colors.white, BlendMode.srcIn),
+          ),
+        ),
+        centerTitle: true,
+        title: Text(
+          "Рабочие простанства",
+          maxLines: 1,
+          overflow: TextOverflow.ellipsis,
+          style: TextStyle(
+            fontWeight: FontWeight.w600,
+            color: Colors.white,
+            fontSize: 18.sp,
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: SvgPicture.asset(
+              "assets/icons/add.svg",
+              width: 28.we,
+              height: 28.we,
+              colorFilter:
+                  const ColorFilter.mode(Colors.white, BlendMode.srcIn),
+            ),
+          ),
+        ],
+      ),
+      body: GridView.builder(
+        padding: EdgeInsets.symmetric(horizontal: 13.we, vertical: 16.he),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 13.we,
+          mainAxisSpacing: 13.we,
+          childAspectRatio: 1.6,
+        ),
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          return Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8.r),
+              color: Colors.blue,
+            ),
+            child: Center(
+              child: Text(
+                'Item $index',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
